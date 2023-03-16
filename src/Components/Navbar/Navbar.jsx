961a23ca5c2d2280e166665693fd77b610a7f8f3
Shubham Vishwakarma <sv773460@gmail.com>
2023-03-17 00:45:08 +0530

    css improved

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { RxHamburgerMenu } from 'react-icons/rx'
 import { Link } from "react-router-dom";
 const Navbar = () => {
+
+  const [sidebarActive, setSidebarActive] = useState(false)
+
   return (
-    <div className="navbar">
-      <div className="nav--name">
-        <Link to="/">Shubham Vishwakarma</Link>
+    <>
+      {/* desktop */}
+      <div className="navbar">
+        <div className="nav--name">
+          <Link to="/">Shubham Vishwakarma</Link>
+        </div>
+        <ul className="nav-navi">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/blogs">
+            {" "}
+            <li>Blog</li>
+          </Link>
+          <Link to="/projects">
+            <li>Projects</li>
+          </Link>
+          <Link to="/experience">
+            <li>Experience</li>
+          </Link>
+        </ul>
       </div>
-      <ul className="nav-navi">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/blogs">
-          {" "}
-          <li>Blog</li>
-        </Link>
-        <Link to="/projects">
-          <li>Projects</li>
-        </Link>
-        <Link to="/experience">
-          <li>Experience</li>
-        </Link>
-      </ul>
-    </div>
+
+      {/* mobile */}
+      <div className="navbar-mobile">
+        <div className="nav">
+          <div className="nav--name">
+            <Link to="/">Shubham Vishwakarma</Link>
+          </div>
+          <RxHamburgerMenu onClick={() =>
+            setSidebarActive(true)} />
+        </div>
+
+
+        {sidebarActive &&
+          (<ul className="nav-navi">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/blogs">
+              {" "}
+              <li>Blog</li>
+            </Link>
+            <Link to="/projects">
+              <li>Projects</li>
+            </Link>
+            <Link to="/experience">
+              <li>Experience</li>
+            </Link>
+          </ul>)}
+      </div>
+
+    </>
   );
 };
 
