85b582725b3a99e44bdfe452d826eb4a09b3c2af
Shubham Vishwakarma <80101565+ShubhamAXS19@users.noreply.github.com>
2023-04-23 10:11:02 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,9 +12,9 @@ const Home = () => {
       <img src={Wallpaper} alt="" />
       <h2>Shubham Vishwakarma</h2>
       <p>
-        Hi 👋, I’m Shubham, a student at the Dwarkadas J. Sanghvi College of
+        Hi 👋, I’m Shubham, a student at Dwarkadas J. Sanghvi College of
         Enginerring. I’m passionate about <b>Backend, Cloud </b> and{" "}
-        <b>DevOps</b>. Along with my Undergrad studies, I work as a freelancer.
+        <b>DevOps</b>. Along with my Undergrad studies, I work as a <b> Backend Developer</b>  at Exampil Pvt. Ltd. an Edtech startup based in Mumbai, India.
       </p>
       <ul>
         <a href="sv773460@gmail.com">
